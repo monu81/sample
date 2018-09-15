d15b6d7dadaa2e5ef71c446f6c87a99a736744eb
shrivastava <shrivastava@srivasak>
2018-09-15 16:08:32 +0530

    modified main file.

--- javasamples/src/javasamples/Main.java
+++ javasamples/src/javasamples/Main.java
@@ -2,6 +2,7 @@ package javasamples;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("output-1");
+		System.out.println("output-1.");
+		System.out.println("output-2, commit-2 from local.");
 	}
 }
