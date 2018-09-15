9c88373a9887d7c45a2af2475c8ad469fbd94d69
shrivastava <shrivastava@srivasak>
2018-09-15 16:09:09 +0530

    mdified main file..

--- javasamples/src/javasamples/Main.java
+++ javasamples/src/javasamples/Main.java
@@ -3,5 +3,7 @@ package javasamples;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("output-1");
+		System.out.println("output-2, from 2nd wrkspace.");
+		
 	}
 }
